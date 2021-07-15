5c39ecd7dc4b88d36f52118afe3ed4d15e6d5a49
rumihsieh <87258074+rumihsieh@users.noreply.github.com>
2021-07-15 14:20:11 +0800

    Create average.py

---
+++ average.py
@@ -0,0 +1,16 @@
+data = []
+count = 0
+with open('review.txt', 'r') as f:
+	for line in f:
+		data.append(line)
+		count += 1
+		if count % 1000 = 0:
+			print(len(data))
+print('檔案讀取完了，總共有',len(data),'筆資料')
+print(data[0])
+print('---------------------')
+
+sum_len = 0
+for d in data:
+	sum_len = sum_len + len(d)
+print('留言的平均長度為：', sum_len/len(data))
