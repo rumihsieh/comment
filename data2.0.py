0c1826925445fc942254ff34e894be92385e532f
rumihsieh <87258074+rumihsieh@users.noreply.github.com>
2021-07-15 14:30:06 +0800

    Create data2.0.py

---
+++ data2.0.py
@@ -0,0 +1,27 @@
+data = []
+count = 0
+with open('review.txt', 'r') as f:
+	for line in f:
+		data.append(line)
+		count += 1
+		if count % 1000 = 0:
+			print(len(data))
+print('檔案讀取完了，總共有',len(data),'筆資料')
+print(data[0])
+print('---------------------')
+
+sum_len = 0
+for d in data:
+	sum_len = sum_len + len(d)
+print('留言的平均長度為：', sum_len/len(data))
+
+
+new = []
+for d in data:
+	if len(d) < 100:
+		new.append(d)
+print('一共有',len(new),'筆留言長度小於一百的資料')
+print(new[0])
+print(new[1])
+
+
