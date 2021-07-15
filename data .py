d218a893c4c2ee96a860f8642af18759a2397e88
rumihsieh <87258074+rumihsieh@users.noreply.github.com>
2021-07-15 14:13:46 +0800

    Add files via upload

---
+++ data .py
@@ -0,0 +1,11 @@
+data = []
+count = 0
+with open('review.txt', 'r') as f:
+	for line in f:
+		data.append(line)
+		count += 1
+		if count % 1000 = 0:
+			print(len(data))
+print('檔案讀取完了',len(data))
+print(data[0])
+print('---------------------')
